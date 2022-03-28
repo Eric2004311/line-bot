f88b5705c0757acd8450c78b5fa05d32424d8ab4
Eric <eric2004311@yahoo.com.tw>
2022-03-28 19:06:35 +0800

    add proc and req

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('017Ztelu1iHkntih/JEUALbYGT3WWvvDq4xxr9+JSVIQRHoFsz7zYCMp9smy1RKXASCdusmlELRDkPikzWtNb4PBvCF8yNZuMANxhcEMs9CYWs3/qPSb8X5AMkyqpndr3zLXEHK+OYowJ/cTnvcE7AdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('f0fcee652263244dfccb18e4001c4f49')
 
 
 @app.route("/callback", methods=['POST'])
